23c522b96df96d159ac9f52c8bbc5a6f7c67ebc0
David <davidnahum92@gmail.com>
2020-06-13 15:43:58 +0300

    server added, rdy to deploy

---
+++ build/precache-manifest.a0dfd9b4a758b842f8b640edbdc1f5ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9c093e7db35457bbd321fb0a464dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01f36bcf126d1ab1c5a4",
+    "url": "/static/css/main.796bc509.chunk.css"
+  },
+  {
+    "revision": "2e1b8732c9c7b4550f64",
+    "url": "/static/js/2.2b79c352.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.2b79c352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f36bcf126d1ab1c5a4",
+    "url": "/static/js/main.91a43a78.chunk.js"
+  },
+  {
+    "revision": "1a46ae56de6b66dadf8f",
+    "url": "/static/js/runtime-main.5a900a63.js"
+  },
+  {
+    "revision": "e400be69095759c6d014bff53a3bd938",
+    "url": "/static/media/cool-background.e400be69.png"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port);
